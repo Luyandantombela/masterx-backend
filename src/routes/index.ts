f406c3380641a059853dde5d85efa9178ceed0e9
Luyandantombela <121224861+Luyandantombela@users.noreply.github.com>
2026-06-01 16:12:17 +0200

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,12 @@
-//YUYKJB
+import { Router, type IRouter } from "express";
+import healthRouter from "./health";
+import masterxRouter from "./masterx";
+import aiRouter from "./ai";
+
+const router: IRouter = Router();
+
+router.use(healthRouter);
+router.use(masterxRouter);
+router.use(aiRouter);
+
+export default router;
